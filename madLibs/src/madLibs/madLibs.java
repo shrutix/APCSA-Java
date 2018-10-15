792829d6627d3453a27166ae84a7deeeb9defd6c
Shruti J <37284513+shrutix@users.noreply.github.com>
2018-10-14 19:22:53 -0700

    Add files via upload

---
+++ madLibs/src/madLibs/madLibs.java
@@ -0,0 +1,275 @@
+//Shruti J
+
+//period 3
+
+package madLibs;
+
+import java.util.Scanner;
+
+public class madLibs {
+
+	public static void main (String[] args)
+	{
+	
+	System.out.println("Welcome to Madlibs!");	
+		
+	//input 1
+	String p;
+	Scanner pen = new Scanner(System.in);
+	
+	System.out.println("Are you a boy or girl?");
+	p = pen.next();
+	pen.nextLine();
+
+	//input 2
+	String a;
+	
+	System.out.println("Would you rather live in the forest or mountains?");
+	a = pen.next();
+	pen.nextLine();
+	
+	//input 3
+	String b;
+	
+	System.out.println("What is your first name?");
+	b = pen.next();
+	pen.nextLine();
+	
+	//input 4
+	int c;
+		
+	System.out.println("How old are you?");
+	c = pen.nextInt();
+	pen.nextLine();
+		
+	//input 5
+	String d;
+	
+	System.out.println("Prefered pronoun? He/She/They");
+	d = pen.next();
+	pen.nextLine();
+	
+	//input 6
+	String e;
+	
+	System.out.println("What type of house is your dream home? eg. mansion, cottage, etc");
+	e = pen.next();
+	pen.nextLine();
+	
+	//input 7
+	String f;
+	
+	System.out.println("Prefered pronoun? he/she/they");
+	f = pen.next();
+	pen.nextLine();
+	
+	//input 8
+	String g;
+	
+	System.out.println("Name one of your hobbies (and add a present tense verb before it). eg. playing drums");
+	g = pen.next();
+	pen.nextLine();
+	
+	//input 9
+	int h; 
+	
+	System.out.println("Give a whole number between 1 and 10.");
+	h = pen.nextInt();
+	pen.nextLine();
+	
+	//input 10
+	int i;
+	
+	System.out.println("Give another whole number between 1 and 10.");
+	i = pen.nextInt();
+	pen.nextLine();
+	
+	//calculation
+	int j;
+	
+	j = h + i;
+	
+	//input 11
+	String k ;
+	
+	System.out.println("Preferred pronoun? he/she/they");
+	k = pen.next();
+	pen.nextLine();
+	
+	//input 12
+	String l;
+	
+	System.out.println("Enter an adjective.");
+	l = pen.next();
+	pen.nextLine();
+	
+	//input 13
+	String m;
+	
+	System.out.println("Light or sound?");
+	m = pen.next();
+	pen.nextLine();
+	
+	//input 14
+	String n ;
+	
+	System.out.println("Preferred pronoun? he/she/they");
+	n = pen.next();
+	pen.nextLine();
+	
+	//input 15
+	String o;
+	
+	System.out.println("Enter an adjective.");
+	o = pen.next();
+	pen.nextLine();
+	
+	//input 16
+	String q;
+	
+	System.out.println("Name an animal (plural form).");
+	q = pen.next();
+	pen.nextLine();
+	
+	//input 17
+	String r ;
+	
+	System.out.println("Preferred pronoun? he/she/they");
+	r = pen.next();
+	pen.nextLine();
+	
+	//input 18
+	String s;
+	
+	System.out.println("Enter an adjective in past tense form. eg. shocked");
+	s= pen.next();
+	pen.nextLine();
+	
+	//input 19
+	String t ;
+	
+	System.out.println("Preferred pronoun? hers/his/theirs");
+	t = pen.next();
+	pen.nextLine();
+	
+	//input 20
+	String u;
+	
+	System.out.println("Your first name again.");
+	u = pen.next();
+	pen.nextLine();
+	
+	//input 21
+	String v; 
+	
+	System.out.println("Name an object.");
+	v = pen.next();
+	pen.nextLine();
+	
+	//input 22
+	String w; 
+	
+	System.out.println("Give a noise. eg. thud, fart");
+	w = pen.next();
+	pen.nextLine();
+	
+	//input 23
+	String x;
+	
+	System.out.println("Name an object in plural form.");
+	x = pen.next();
+	pen.nextLine();
+	
+	//input 24
+	int y;
+	
+	System.out.println("Give a whole number between 1 and 10.");
+	y = pen.nextInt();
+	pen.nextLine();
+	//math altered number
+	y = 100 * y;
+	
+	//input 25
+	String z;
+	
+	System.out.println("Your first name again.");
+	z = pen.next();
+	pen.nextLine();
+	
+	//input 26
+	String aa;
+	
+	System.out.println("Preferred pronoun? herself/himself/themselves");
+	aa = pen.next();
+	pen.nextLine();
+	
+	//input 27
+	String bb;
+	
+	System.out.println("Give an adverb.");
+	bb = pen.next();
+	pen.nextLine();
+	
+	//input 28
+	String cc ;
+	
+	System.out.println("Preferred pronoun? him/her/them");
+	cc = pen.next();
+	pen.nextLine();
+	
+	//input 29
+	String dd;
+	
+	System.out.println("Name a place.");
+	dd = pen.next();
+	pen.nextLine();
+	
+	//input 30
+	String ee;
+	
+	System.out.println("Give an adverb.");
+	ee = pen.next();
+	pen.nextLine();
+	
+	//input 31
+	int ff;
+	
+	System.out.println("Give a whole number between 2 and 15.");
+	ff = pen.nextInt();
+	pen.nextLine();
+	//math altered number
+	ff = ff * 1000;
+	
+	//story line
+	
+	System.out.println();
+	
+	System.out.println("A Mystical Life in a Mystical Place");
+	
+	System.out.println();
+	
+	System.out.println("Once" + " " + "upon" + " " + "a" + " " + "time" + " " + "there" + " " + "was" + " " + "a" + " " + p + " " + "living" + " " + "in" + " " + "the" + " " + "mystical" + " " + a + " " + "of" + " " + "Itty" + " " + "Bitty" + " " + "Boo" + "." + " " + "Their" + " " + "name" + " " + "was" + " " + b + " " + "and" + " " + "was" + " " + c + " " + "years" + " " + "old" + "." + " " + d + " " + "lived" + " " + "in" + " " + "solidary" + " " + "in" + " " + "the" + " " + "confines" + " " + "of" + " " + "their" + " " + e + "." + " " + "Even" + " " + "through" + " " + b + " " + "got" + " " + "lonely" + " " + "at" + " " + "times" + "," + " " + f + " " + "spent" + " " + "lots" + " " + "of" + " " + "time" + " " + g + "." + " " + "At" + " " + "least" + " " + j + " " + "hours" + " " + "a" + " " + "day" + " " + "was" + " " + "spent" + " " + g + "." + " " + "Finally" + " " + b + " " + "decided" + " " + "that" + " " + "solitary" + " " + "living" + " " + "was" + " " + "boring" + " " + "and" + " " + "set" + " " + "out" + " " + "to" + " " + "find" + " " + "some" + " " + "friends" + "." + " " + "While" + " " + "strolling" + " " + "through" + " " + "the" + " " + a + " " + "," + " " + k + " " + "found" + " " + "a" + " " + l + " " + m + "." + " " + "After" + " " + "following" + " " + "that" + " " + m + "," + " " + n + " " + "found" + " " + "a" + " " + "large" + " " + "group" + " " + "of" + " " + o + " " + "talking" + " " + q + "." + " " + "At" + " " + "first" + " " + r + " " + "was" + " " + s + "," + " " + "but" + " " + "then" + " " + "realized" + " " + "that" + " " + "they" + " " + "could" + " " + "be" + " " + "the" + " " + "new" + " " + "friends" + " " + "of" + " " + t + "." + " " + "Quietly" + " " + "watching" + " " + "the" + " " + q + " " + "at" + " " + "first" + "," + " " + u + " " + "then" + " " + "came" + " " + "from" + " " + "shadows" + " " + "but" + " " + "accidentally" + " " + "stepped" + " " + "on" + " " + "a" + " " + v + " " + "which" + " " + "made" + " " + "a" + " " + "loud" + " " + w + "." + " " + "The" + " " + q + " " + "quickly" + " " + "turned" + " " + "around" + " " + "and" + " " + "dropped" + " " + "all" + " " + "their" + " " + x + "." + " " + "There" + " " + "were" + "at" + " " + "least" + " " + y + " " + "of" + " " + "them" + "." + " " + z + " " + "introduced" + " " + aa + " " + "to" + " " + "them" + "." + " " + "The" + " " + q + " " + bb + " " + "became" + " " + "friends" + " " + "with" + " " + cc + " " + "and" + " " + "ran" + " " + "to" + " " + "the" + " " + dd + " " + "together" + "." + " " + "They" + " " + "all" + " " + "lived" + " " + ee + " " + "ever" + " " + "after" + " " + "together" + " " + "with" + " " + ff + " " + "more" + " " + "animals" + ".");
+	
+	System.out.println("The end!");
+	
+	
+	
+	
+	
+	}
+}
+
+
+/*
+
+    Once upon a time there was a __1p___ (boy/girl) living in the mystical ___2a____ (forest/mountains) of Itty Bitty Boo. Their name was ____3b___ (name) and was ___4c_ (age) years old . _____5d__ (He/She/They) lived in solitary in the confines of their _____6e____ (dream home). Even though ___3b__ (repeat name) got lonely at times, ____7f___ (he/she/they) spent lots of time ___8g___ (hobby). At least __calc j_____ (number) hours a day was spent ____8g__ (repeat hobby). Finally __3b__ (repeat name) decided that solitary living was boring and set out to find some friends. While strolling through the ___2a__ (repeat forest/mountains), ___11k___ (he/she/they) found a ____12l___ (adjective) _____13m_____ (light/sound). After following that, ___14n___ (he/she/they) found a large group of ___15o___ (adjective) talking ___16q___ (animals). At first ___17r___ (he/she/they) was ___18s___ (adjective past tense), but then realized that they could be new friends of ____19t_____ (hers/his/theirs). Quietly watching the ___16q___ (repeat animals) at first, ____20u____ (name) then came out from the shadows but accidentally stepped on a ____21v____ (object) which made a loud ___22w___ (noise). The ___16q___ (repeat animals) quickly turned around and dropped all their ___ 23x____ (objects). There were at least ___24y___ (number) of them. ___25z___ (name) introduced ____26aa___ (herself/himself/themselves) to them. The ___16q___ (repeat animals) ___27bb___ (adverb) became friends with ____28cc____ (her/him/them) and ran to the ____29dd____ (place) together. They all lived ____30ee___ (adverb) ever after together with ___31ff___ (number) more animals.
+
+
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
